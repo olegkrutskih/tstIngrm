e684913207c41529eace942da93b390b70161b7a
Олег Круцких <oleg.krutskih@icloud.com>
2016-06-28 20:44:40 +0300

    Initial Commit

---
+++ tstIngrm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tstIngrm
+//
+//  Created by Круцких Олег on 28.06.16.
+//  Copyright © 2016 Круцких Олег. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
